2135cb925ddba7f832d25d120b399262639cffed
Vinicius Reif <vinicius.reif@senior.com.br>
2019-04-03 13:28:04 -0300

    Alterado horario para exemplo

--- src/main/java/MainServidor.java
+++ src/main/java/MainServidor.java
@@ -20,13 +20,13 @@ public class MainServidor {
             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
             
             // Servidor 1
-            HorarioServidor hs1 = new HorarioServidorImpl(sdf.parse("07:20:10"));
+            HorarioServidor hs1 = new HorarioServidorImpl(sdf.parse("07:30:10"));
             Registry registry1 = LocateRegistry.createRegistry(PORTA_SERVIDOR_1);
             registry1.rebind("HorarioServidorImpl", hs1);
             System.out.println("Servidor 1 iniciado...");
             
             // Servidor 2
-            HorarioServidor hs2 = new HorarioServidorImpl(sdf.parse("09:30:15"));
+            HorarioServidor hs2 = new HorarioServidorImpl(sdf.parse("09:20:15"));
             Registry registry2 = LocateRegistry.createRegistry(PORTA_SERVIDOR_2);
             registry2.rebind("HorarioServidorImpl", hs2);
             System.out.println("Servidor 2 iniciado...");
